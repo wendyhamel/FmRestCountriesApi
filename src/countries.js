0d38ab07eb21adfc218c682d7a165873d9816441
wendyhamel <whamel@it4web.nl>
2022-10-12 15:59:24 +0200

    Cleaned up js code: Just one fetch; improved names; in region dropdown -> not showing active region replaced by checkmark for active region.

--- src/countries.js
+++ src/countries.js
@@ -2,58 +2,57 @@ window.countriesJS = function() {
     return {
         darkMode : Alpine.$persist(true),
         isLoading : true,
-        isLoadingCountry : true,
         api : 'https://restcountries.com/v3.1',
         fields : 'cca3,name,flags,capital,population,region,subregion,currencies,borders,languages,tld',
-        regions : ['all', 'africa', 'america', 'asia', 'europe', 'oceania'],
-        selectedRegion: Alpine.$persist('Filter by Region'),
+        regions : ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'],
+        selectedRegion: Alpine.$persist('All'),
         filtersOpen : false,
         countries : [],
         singleCountry : false,
         showCountry : false,
         searchCountry: '',
 
-        all() {
+        getAll() {
             fetch(this.api + '/all?fields=' + this.fields, {cache: 'no-cache'})
                 .then(response => response.json())
                 .then(data => {this.countries = data; this.isLoading = false})
             return this.countries
         },
 
-        byRegion(region) {
-            fetch(this.api + '/region/' + region + '?fields=' + this.fields, {cache: 'no-cache'})
-                .then(response => response.json())
-                .then(data => {this.countries = data; this.isLoading = false})
-            return this.countries
-        },
+        getCountries() {
+            if (this.selectedRegion !== 'All') {
+                let showCountries = this.countries;
 
-        single(cca3) {
-            fetch(this.api + '/alpha/' + cca3 + '?fields='  + this.fields, {cache: 'no-cache'})
-                .then(response => response.json())
-                .then(data => {this.singleCountry = data; this.isLoadingCountry = false})
-            return this.singleCountry
-        },
-
-        searchByName(name) {
-            if (this.searchCountry === '' || this.searchCountry === false) {
-                this.filterCountries('all')
-            } else {
-                fetch(this.api + '/name/' + name + '?fields='  + this.fields, {cache: 'no-cache'})
-                    .then(response => response.json())
-                    .then(data => {this.countries = data; this.isLoading = false})
-                return this.countries
+                showCountries = this.countries.filter((country) => {
+                    return country.region === this.selectedRegion;
+                });
+                if (this.searchCountry) {
+                    showCountries = showCountries.filter((country) => {
+                        return country.name.common.toLowerCase().includes(this.searchCountry.toLowerCase())
+                    });
+                }
+                return showCountries;
             }
+            if (this.searchCountry) {
+                return this.countries.filter((country) => {
+                    return country.name.common.toLowerCase().includes(this.searchCountry.toLowerCase())
+                });
+            }
+            return this.countries;
         },
 
-        filterCountries(region) {
-            if(region === 'all') {
-                this.all()
-                this.searchCountry = ''
-            } else {
-                this.byRegion(region)
-                this.searchCountry = ''
-            }
+        getSingleCountry(cca3) {
+            return this.singleCountry = this.countries.filter((country) => {
+                return country.cca3 === cca3;
+            })[0]
+        },
+
+        getBorderCountriesFor(countryCodes) {
+            let countryCodesArray = Object.values(countryCodes);
+
+            return this.countries.filter((country) => {
+                return countryCodesArray.includes(country.cca3);
+            })
         }
-
     }
 }
